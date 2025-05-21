ba7fd0e81fe63ef8bc90b5c98acc235760005f1b
Ulises Ribeiro <Ulises Ribeiro>
2025-05-20 22:33:52 -0300

    add: menu responsivo adicionado

---
+++ app.js
@@ -0,0 +1,6 @@
+const menu = document.getElementById('nav-links')
+const icone = document.getElementById('menu-toggle')
+
+icone.addEventListener('click', function (){
+    menu.classList.toggle('mostrar')
+})
